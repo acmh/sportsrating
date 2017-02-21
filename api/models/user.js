098cb9a0411ecd59d3643144048c0c9f0a5b9787
Nilson <nlj@cin.ufpe.br>
2017-02-21 15:32:59 -0300

    Add user model

---
+++ api/models/user.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+	cpf: String,
+	email: String,
+	nome: String,
+	idade: Number
+});
+
+mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -7,6 +7,7 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -15,13 +16,52 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
+
+
+
+var User = require('./api/models/user.js');
+
+
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+	
+    res.status(200).json({ message: 'hooray! welcome to our api!' });
+});
+
+router.post('/users/create', function(req, res){
+	var _nome = req.body.nome;
+	var _cpf = req.body.cpf;
+	var _email = req.body.email;
+	var _idade = req.body.idade;
+
+	var usuario = new User();
+	
+	usuario.nome = _nome;
+	usuario.cpf = _cpf;
+	usuario.email = _email;
+	usuario.idade = _idade;
+
+	usuario.save(function(err){
+		if(err){
+			res.status(500).json({
+				sucesso: false,
+				erro: err.message
+			});
+		}else{
+
+			res.status(200).json({
+				sucesso: true,
+				mensagem: "Usuario cadastrado com sucesso"
+			});
+
+
+		}
+	});
+	
 });
 
 // more routes for our API will happen here
@@ -29,6 +69,7 @@ router.get('/', function(req, res) {
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
+mongoose.connect("mongodb://127.0.0.1:27017/db");
 
 // START THE SERVER
 // =============================================================================
