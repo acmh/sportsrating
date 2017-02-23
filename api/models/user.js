0590fcb9276a1343d81999fdcfb2199866b963f9
acmh <acmh@cin.ufpe.br>
2017-02-23 16:23:12 -0300

    Authentication implemented

--- api/models/user.js
+++ api/models/user.js
@@ -1,11 +1,38 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
 var UserSchema = new Schema({
-	cpf: String,
-	email: String,
-	nome: String,
-	idade: Number
+	email: { type: String, unique: true, trim: true },
+	name: {type: String, trim: true},
+	password: String,
+	salt: String
 });
+
+
+
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.password = crypto.pbkdf2Sync(password, this.salt, 1000, 64,'sha512').toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+  return this.password === hash;
+};
+
+//Generate a token with some extra informations (like expire time)
+UserSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime() / 1000)
+  }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
+};
 
 module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
+var jwt = require('jsonwebtoken');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -28,81 +29,129 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	
+
     res.status(200).json({ message: 'hooray! welcome to our api!' });
 });
 
-router.post('/users/create', function(req, res){
-	var _nome = req.body.nome;
-	var _cpf = req.body.cpf;
-	var _email = req.body.email;
-	var _idade = req.body.idade;
 
-	var usuario = new User();
-	
-	usuario.nome = _nome;
-	usuario.cpf = _cpf;
-	usuario.email = _email;
-	usuario.idade = _idade;
+router.post('/users/register', function(req, res){
+	var user = new User();
+	user.name = req.body.name;
+	user.email = req.body.email;
+	user.setPassword(req.body.password);
 
-	usuario.save(function(err){
-		if(err){
-			res.status(500).json({
-				sucesso: false,
-				erro: err.message
-			});
-		}else{
+	user.save(function(err){
+			if(err){
+				res.status(500).json({
+					message: err.message
+				})
+			}else{
+				User.findOne({email: user.email}, function(err, user){
+					if(err){
+						res.status(500).json({
+							message: err.message
+						})
+					}
 
-			res.status(200).json({
-				sucesso: true,
-				mensagem: "Usuario cadastrado com sucesso"
-			});
+					if(!user){
+						res.status(401).json({
+							message: 'Authentication failed. User not found.'
+						});
+					}else{
+						if(!user.validPassword(req.body.password)){
+							res.status(401).json({
+								success: false,
+								message: 'Authentication failed. Wrong password.'
+							});
+						}else{
+							var token = user.generateJwt();
+
+							res.status(200).json({
+							  	"token" : token,
+						   		"username" : user.name
+							});
+						}
+					}
 
 
-		}
-	});
-	
-});
-
-router.get('/users/list', function(req, res){
-	User.find({}, function(err, usuarios){
-		if(err){
-			res.status(500).json({
-				sucesso: false,
-				erro: err.message
-			});		
-		}else{
-			res.status(200).json({
-				sucesso: true,
-				dados: usuarios
-			})
-		}
+				})
+			}
 	})
+
 })
 
-router.get('/users/search', function(req, res){
-	var _nome = req.query.nome;
-	User.find({nome: new RegExp(_nome, "i")}, function(err, usuarios){
+router.post('/users/login',function(req,res){
+	var _email = req.body.email;
+	User.findOne({email: _email}, function(err, user) {
 		if(err){
-			res.status(500).json({
-				sucesso: false,
-				erro: err.message
-			});
-		}else{
-			res.status(200).json({
-				sucesso: true,
-				dados: usuarios
-			})	
-		}		
-		
-	})
+			res.status(404).json(err);
+			return;
+		}
 
+		//If there is no user with this email
+		if(!user){
+			res.status(401).json({
+				success: false,
+				message: 'Authentication failed. User not found.'
+			});
+
+		}else if(user){
+			if(!user.validPassword(req.body.password)){
+				res.status(401).json({
+					success: false,
+					message: 'Authentication failed. Wrong password.'
+				});
+			}else{
+				var token = user.generateJwt();
+				res.status(200);
+				res.json({
+				  	"token" : token,
+			   		"username" : user.name
+				});
+			}
+		}
+	});
 });
 
+router.get('/users/profile',function(req,res){
+	if(req.decoded){
+		res.status(200).json({
+			name: req.decoded.name
+		})
+	}else{
+		res.status(404).json({
+			message: "Profile not found"
+		})
+	}
+})
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
+
+app.use('/api/users/profile', function(req, res, next){
+	var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+	if(token){
+			jwt.verify(token, 'MY_SECRET', function(err, decoded){
+				if(err){
+					res.status(500).json({
+							message: 'Failed to authenticate token.'
+					})
+				}else{
+					req.decoded = decoded;
+					 next();
+				}
+			})
+	}else{
+		res.status(403).json({
+  		message: 'No token provided.'
+  	});
+	}
+
+
+})
+
 app.use('/api', router);
 mongoose.connect("mongodb://127.0.0.1:27017/db");
 
