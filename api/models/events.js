dca8eb16008f5ecb4d303733bcbbe964ca11a761
Lucas Cavalcanti <lhcs@cin.ufpe.br>
2017-03-02 17:16:08 -0300

    date fixes

--- api/models/events.js
+++ api/models/events.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 var EventsSchema = new Schema({
 	dateStart: { type: Date, default: Date.now},
-	dateEnd: { type: Date, default: Date.now },
+	dateEnd: { type: Date, default: nowPlusOneHour },
 	name: { type: String },
 	price: { type: Number },
 	weather: { type: String },
@@ -15,5 +15,9 @@ var EventsSchema = new Schema({
 	usersPaid: [{ type: Schema.Types.ObjectId, ref: 'User', default: {}}]
 });
 
-
+var nowPlusOneHour = function(){
+    var timeObject = new Date();
+    timeObject.setTime(timeObject.getTime() + 1000 * 60 * 60);
+    return timeObject;
+};
 module.exports = mongoose.model('Events', EventsSchema);
