02865715378cfe9bd14e2e70c7991722a26ca7df
Gabriel Bandeira <gmb@cin.ufpe.br>
2017-03-02 17:09:13 -0300

    Fix minor errors

--- api/models/events.js
+++ api/models/events.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 var EventsSchema = new Schema({
 	dateStart: { type: Date, default: Date.now},
-	dateEnd: { type: Date, default: Date.now + 60*60*1000 },
+	dateEnd: { type: Date, default: Date.now },
 	name: { type: String },
 	price: { type: Number },
 	weather: { type: String },
--- api/routes/events.routes.js
+++ api/routes/events.routes.js
@@ -2,9 +2,6 @@ var events = require('../../api/controllers/events.controller');
 
 module.exports = function(app) {
 
-  app.route('/api/events/delete')
-		.post(events.delete);
-
 	app.route('/api/event/create')
 		.post(events.create);
   
