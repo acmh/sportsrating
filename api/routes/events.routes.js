91965e8601787ec485c4ef407f1982c267f9fae5
Roberto Fernandes <rcf6@cin.ufpe.br>
2017-03-02 16:55:24 -0300

    adding events schema in mongoose

--- api/routes/events.routes.js
+++ api/routes/events.routes.js
@@ -1,5 +1,6 @@
 var events = require('../../api/controllers/events.controller');
 
 module.exports = function(app) {
-  
+  app.route('/api/events/delete')
+		.post(events.delete);
 };
--- config/mongoose.js
+++ config/mongoose.js
@@ -4,8 +4,8 @@ var config = require('./config'),
 module.exports = function() {
 	//mongoose.Promise = global.Promise;
 	var db = mongoose.connect(config.db);
-
+	//Adding Schemas to DB
 	require('../api/models/user.js');
-
+	require('../api/models/events.js');
 	return db;
 };
